4c18a3916de782263f1648680b6d8eabf28e6bb1
AnasBenba <anasbenba123@gmail.com>
2023-05-15 03:15:59 -0700

    fix the algorithme

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,61 +8,36 @@
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *ptr = *head;
-	listint_t *ptr1 = *head;
-	int len = 1, value1 = 0, value2 = 0;
+	listint_t *slow = *head, *fast = *head, *prev = NULL, *next = NULL, *tmp = NULL;
 
-	if (*head == NULL)
-	{
-		return (1);
-	}
-	while (ptr1->next != NULL)
-	{
-		ptr1 = ptr1->next;
-		len++;
-	}
-	if (len % 2 != 0)
-	{
-		int i = 1;
-
-		while (i <= len / 2)
-		{
-			value1 += ptr->n;
-			ptr = ptr->next;
-			i++;
-		}
-		i++;
-		ptr = ptr->next;
-		while (i <= len)
-		{
-			value2 += ptr->n;
-			ptr = ptr->next;
-			i++;
-		}
-	}
-	else
-	{
-		int i = 1;
-
-		while (i <= len / 2)
-		{
-			value1 += ptr->n;
-			ptr = ptr->next;
-			i++;
-		}
-		while (i <= len)
-		{
-			value2 += ptr->n;
-			ptr = ptr->next;
-			i++;
-		}
-	}
-	if (value1 == value2)
-	{
-		return (1);
-	}
-	else
+	if (*head == NULL || (*head)->next == NULL)
 	{
 		return (0);
 	}
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	tmp = slow;
+	while (tmp)
+	{
+		next = tmp->next;
+		tmp->next = prev;
+		prev = tmp;
+		tmp = next;
+	}
+
+	tmp = prev;
+	while (tmp)
+	{
+		if ((*head)->n != tmp->n)
+		{
+			return (0);
+		}
+		*head = (*head)->next;
+		tmp = tmp->next;
+	}
+
+	return (1);
 }
