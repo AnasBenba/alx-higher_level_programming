328fba2a08441e3fa3eddf035e70de2f53a4947e
AnasBenba <anasbenba123@gmail.com>
2023-05-17 14:35:59 -0700

    fix code

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if type(roman_string) != str or len(roman_string) == 0:
+        return 0
     last = roman_string[0]
     result = 0
     roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
