a0940215ce7a97397f3fa78c1850a029a2b748ce
AnasBenba <anasbenba123@gmail.com>
2023-05-29 03:05:16 -0700

    Safe list printing

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             if i < x:
                 print(elm, end = "")
                 i += 1
-        print("\n")
-    except:
-        print("Error")
+        print()
+    except TypeError:
+        pass
     return i
