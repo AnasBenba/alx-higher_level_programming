35e4901f2f57350b25ae002a638e2611aa9c9ca7
AnasBenba <anasbenba123@gmail.com>
2023-05-29 03:07:52 -0700

    fix code

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
     try:
         for elm in my_list:
             if i < x:
-                print(elm, end = "")
+                print(elm, end="")
                 i += 1
         print()
     except TypeError:
