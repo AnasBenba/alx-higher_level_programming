f819ebac3a985914203a4743880d698d75ef9d5f
AnasBenba <anasbenba123@gmail.com>
2023-06-06 15:06:03 -0700

    fix code

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 class LockedClass:
+    """A class that prevents the creation of new instance
+    attributes, except for 'first_name'."""
+
     def __setattr__(self, name, value):
+        """
+        Set the value of an attribute on
+        the LockedClass instance.
+
+        Args:
+            name (str): The name of the attribute.
+            value: The value to be assigned to the attribute.
+
+        Raises:
+            AttributeError: If the attribute
+            name is not 'first_name'.
+        """
         if name != 'first_name':
-            raise AttributeError("'LockedClass' object has no attribute 'last_name'")
+            raise AttributeError("'LockedClass' object \
+has no attribute 'last_name'")
         super().__setattr__(name, value)
