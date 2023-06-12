c9b32d1f73fa4c613667aaea01ae046c0412f9dc
AnasBenba <anasbenba123@gmail.com>
2023-06-12 11:01:39 -0700

    class MyInt that inherits from int

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
+"""class MyInt that inherits from int"""
+
 
 class MyInt(int):
-
     def __eq__(self, value):
+        """
+        Overrides the equality operator '=='.
+
+        Args:
+            value: The value to compare with.
+
+        Returns:
+            bool: Always returns False, indicating instances of MyInt are never equal to any value.
+        """
         return False
 
     def __ne__(self, value):
+        """
+        Overrides the inequality operator '!='.
+
+        Args:
+            value: The value to compare with.
+
+        Returns:
+            bool: Always returns True, indicating instances of MyInt are always not equal to any value.
+        """
         return True
