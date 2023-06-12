16a42703c5654910fe0bead86b55f746780f342c
AnasBenba <anasbenba123@gmail.com>
2023-06-12 04:29:06 -0700

    fix code

--- 0x0A-python-inheritance/5-base_geometry.py
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
 class BaseGeometry:
-    """Empty class"""
+    """
+    Base class for geometric shapes.
+
+    This class serves as the foundation for defining different geometric shapes.
+    It provides a common interface and can be used as a superclass for more specific
+    geometric shape classes.
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
     pass
