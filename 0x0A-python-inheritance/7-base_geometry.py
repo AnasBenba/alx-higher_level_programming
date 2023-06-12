a18c219e6c1478d970c2b01e055989d4b6a642b5
AnasBenba <anasbenba123@gmail.com>
2023-06-12 09:01:13 -0700

    fix code

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,6 +15,20 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
+        """
+        Validate if a value is an integer and greater than 0.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+
+        Returns:
+            None
+        """
         if not isinstance(value, int):
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
-"""empty class"""
 
-
-class BaseGeometry:
-    """base geometry class"""
-    def area(self):
-        """
-        Raises:
-            Exception: This method is not implemented in the base class.
-
-        Returns:
-            None
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 class Rectangle(BaseGeometry):
     """
