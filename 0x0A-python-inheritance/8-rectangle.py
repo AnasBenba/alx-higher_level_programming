add9eb649d51ee1b56d86ecb1effd5b6e39f892e
AnasBenba <anasbenba123@gmail.com>
2023-06-12 07:38:10 -0700

    fix code

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -21,8 +21,33 @@ class BaseGeometry:
             raise ValueError("{} must be greater than 0".format(name))
 
 class Rectangle(BaseGeometry):
+    """
+    Rectangle class, inherits from BaseGeometry.
 
+    This class represents a rectangle shape and provides methods for calculating
+    its area and validating the width and height.
+
+    Attributes:
+        None
+
+    Methods:
+        __init__(self, width, height): Initializes a rectangle object with the given width and height.
+    """
     def __init__(self, width, height):
+        """
+        Initializes a rectangle object with the given width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than or equal to 0.
+
+        Returns:
+            None
+        """
         super().integer_validator("width", width)
         super().integer_validator("height", height)
         self._Rectangle__width = width
