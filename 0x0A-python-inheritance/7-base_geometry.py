cb4b12922e87a5bdd81fa2e6062ca617bcf0a633
AnasBenba <anasbenba123@gmail.com>
2023-06-12 09:13:12 -0700

    fix code

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -29,7 +29,7 @@ class BaseGeometry:
         Returns:
             None
         """
-        if type(value) != int
+        if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
