e637f114b51a443b51c70faac9007a3d2903651f
AnasBenba <anasbenba123@gmail.com>
2023-07-19 04:11:42 -0700

    fix code

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -2,4 +2,4 @@
 SELECT tv_showa.title, tv_show_genres.genre_id
 FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+ORDER BY title ASC, genre_id ASC;
