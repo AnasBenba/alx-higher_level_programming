18c1c4df5cc5dd28729648968a9c9713913d07f8
AnasBenba <anasbenba123@gmail.com>
2023-12-12 02:06:20 -0800

    task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if ((w <= 0 || h <= 0) || (w === undefined || h === undefined)) {
+      Object.create(null);
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let output = '';
+      for (let y = 0; y < this.width; y++) {
+        output += 'X';
+      }
+      console.log(output);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
