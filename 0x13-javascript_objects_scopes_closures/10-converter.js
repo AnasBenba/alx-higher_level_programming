8313229167c8e0a316da3b66d76912e250cbf3f1
AnasBenba <anasbenba123@gmail.com>
2023-12-13 00:52:03 -0800

    function that executes x times a function

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  return function conv (num) {
+          let result = '';
+	  let reminder = '';
+	  if (num === 0) {
+      return '0';
+	  } else if (base !== 16) {
+	    while (num > 0) {
+	      reminder = num % base;
+	      result = reminder.toString() + result;
+	      num = num / base;
+	      num = parseInt(num);
+	    }
+	  } else {
+	    while (num > 0) {
+              reminder = num % base;
+	      if (reminder < 10) {
+                result = reminder.toString() + result;
+	      } else {
+                result = String.fromCharCode('A'.charCodeAt(0) + reminder - 10) + result;
+	      }
+            num = num / base;
+            num = parseInt(num);
+            }
+	  }
+	  return result;
+  };
+};
