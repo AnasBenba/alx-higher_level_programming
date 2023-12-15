73709ff1daf4911b23f0cb70881f73cd23416b6c
AnasBenba <anasbenba123@gmail.com>
2023-12-15 16:52:40 +0100

    fix code

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -2,7 +2,7 @@
 """list first State objects from the database hbtn_0e_6_usa"""
 from ast import arg
 from sys import argv
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 from sqlalchemy import create_engine
 
@@ -10,8 +10,7 @@ if __name__ == "__main__":
     data_URL = f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}'
     engine = create_engine(data_URL)
     Base.metadata.create_all(engine)
-    Session = sessionmaker(engine)
-    session = Session()
+    session = Session(engine)
     result = session.query(State).order_by(State.id.asc()).first()
     if result != '':
         print(f'{result.id}: {result.name}')
