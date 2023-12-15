ffd4803f1b1b0f345cc41c6dc4836c2c854d7e9a
AnasBenba <anasbenba123@gmail.com>
2023-12-15 12:10:19 +0100

    add docstring

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,9 +1,27 @@
 #!/usr/bin/python3
+"""
+ connect to MySQL database and retrieves data from the table
+
+ Usage:
+ ./1-filter_states.py <username> <password> <database_name>
+
+ Args:
+ - sys.argv[1]: MySQL username
+ - sys.argv[2]: MySQL password
+ - sys.argv[3]: MySQL database name
+
+ print cities from the database
+"""
+
 import sys
 import MySQLdb
 
 
 def main():
+    """
+    Main function to connect to the database and retrieve
+    data from the 'cities' table
+    """
     usern = sys.argv[1]
     passw = sys.argv[2]
     name = sys.argv[3]
