cf351dcdeaa0017475ba41f67b3a1820a93a3e40
AnasBenba <anasbenba123@gmail.com>
2023-12-15 15:39:22 +0100

    fix code

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,9 +7,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from model_state import Base, State
 
+
 def main():
     """Main function to connect to the database"""
     text = f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}'
+    Base.metadata.create_all(engine)
     engine = create_engine(text)
     session = Session(engine)
     result = session.query(State).order_by(State.id.asc()).all()
