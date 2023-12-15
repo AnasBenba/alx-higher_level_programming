8af71a0af6cc1c0139b2d27bd20691d9b4943292
AnasBenba <anasbenba123@gmail.com>
2023-12-15 11:15:09 +0100

    fix code

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
+"""
+ connect to MySQL database and retrieves data from the table
+
+ Usage:
+ ./1-filter_states.py <username> <password> <database_name>
+
+ Args:
+ - sys.argv[1]: MySQL username
+ - sys.argv[2]: MySQL password
+ - sys.argv[3]: MySQL database name
+
+ print states with a name starts with upper N
+"""
 
 import sys
 import MySQLdb
 
 
 def main():
+    """
+    Main function to connect to the database and retrieve
+    data from the 'states' table
+    """
     usern = sys.argv[1]
     passw = sys.argv[2]
     name = sys.argv[3]
