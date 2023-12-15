fb46d7fecfa257d6db33d4787a71d36e811a3d12
AnasBenba <anasbenba123@gmail.com>
2023-12-15 12:00:25 +0100

    add docstring

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,14 +1,33 @@
 #!/usr/bin/python3
+"""
+ connect to MySQL database and retrieves data from the table
+
+ Usage:
+ ./1-filter_states.py <username> <password> <database_name> <name_searched>
+
+ Args:
+ - sys.argv[1]: MySQL username
+ - sys.argv[2]: MySQL password
+ - sys.argv[3]: MySQL database name
+ - sys.argv[4]: state name searched
+
+ print states where a name matches the argument
+"""
+
 import sys
 import MySQLdb
 
 
 def main():
+    """
+    Main function to connect to the database and retrieve
+    data from the 'states' table (safe from MySQL injection)
+    """
     usern = sys.argv[1]
     passw = sys.argv[2]
     name = sys.argv[3]
     searched = sys.argv[4]
-    q = f'SELECT * FROM states WHERE name = %s ORDER BY states.id'
+    q = f'SELECT * FROM states WHERE BINARY name = %s ORDER BY states.id'
 
 
     db = MySQLdb.connect(host="localhost", user=usern, passwd=passw, db=name)
