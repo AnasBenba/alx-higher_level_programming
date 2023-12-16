f56210532dafc44d7bffc20ba0a7f1efb15884a8
AnasBenba <anasbenba123@gmail.com>
2023-12-16 11:53:05 +0100

    fix code

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -10,6 +10,7 @@ from model_state import Base, State
 if __name__ == "__main__":
     data_URL = f'mysql://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}'
     engine = create_engine(data_URL)
+    Base.metadata.create_all(engine)
     session = Session(engine)
     result = session.query(State).filter(State.name.like('%a%')).all()
     for r in result:
