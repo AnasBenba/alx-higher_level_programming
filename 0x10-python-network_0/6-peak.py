b8a2aa8416daa23ad0ffb7ae0318dce0f1477360
AnasBenba <anasbenba123@gmail.com>
2024-01-05 18:54:17 +0100

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -11,4 +11,16 @@ def find_peak(list_of_integers):
     my_list = list_of_integers
     if len(my_list) == 0:
         return None
-    return max(my_list)
+    if len(my_list) == 1:
+        return my_list[0]
+    if len(my_list) == 2:
+        return max(my_list)
+    
+    mid = len(my_list) // 2
+
+    if my_list[mid] > my_list[mid - 1] and my_list[mid] > my_list[mid + 1]:
+        return my_list[mid]
+    elif my_list[mid] < my_list[mid - 1]:
+        return find_peak(my_list[:mid])
+    else:
+        return find_peak(my_list[mid + 1:])
