cb87925a4e268f5b98c4af9b2c91c85dc7611fea
AnasBenba <anasbenba123@gmail.com>
2024-01-07 10:10:33 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""Python script that fetches 
-https://alx-intranet.hbtn.io/status"""
+"""Python script that fetches"""
 import urllib.request
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
@@ -8,7 +7,7 @@ if __name__ == "__main__":
         content = response.read()
         decoded_content = content.decode('utf-8')
         print("Body response:")
-        print(f"    - type: {type(content)}")
-        print(f"    - content: b'{decoded_content}'")
-        print(f"    - utf-8 content: {decoded_content}")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: b'{decoded_content}'")
+        print(f"\t- utf-8 content: {decoded_content}")
 
