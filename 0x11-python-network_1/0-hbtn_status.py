5908afe521b16c5fd11d69d4a7def00d873b5953
AnasBenba <anasbenba123@gmail.com>
2024-01-07 10:04:41 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import urllib.request
 """Python script that fetches 
 https://alx-intranet.hbtn.io/status"""
+import urllib.request
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
