77608040f5aca56625ae638d8d6d26a81231fe82
AnasBenba <anasbenba123@gmail.com>
2024-01-09 04:11:59 -0800

    fix codr

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,5 +8,12 @@ if __name__ == '__main__':
     url = f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits'
     responde = requests.get(url)
     r = responde.json()
+    j = 0
+    for i in range(len(r)):
+        for j in range(0, j-i-1):
+            date1 = int(r[j]['date'].split('T')[0].split('/', ''))
+            date2 = int(r[j+1]['date'].split('T')[0].split('/', ''))
+            if date1 < date2:
+                r[j], r[j+1] = r[j+1], r[j]
     for i in range(0, 10):
-        print(f"{r[i].get('sha')}: {r[i].get('commit').get('author').get('name')}")
+        print(f"{r[i]['sha']}: {r[i]['commit']['author']['name']}")
