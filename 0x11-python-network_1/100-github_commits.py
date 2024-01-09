012f36e1ee71660bddf62e7d664b65fb8638cc22
AnasBenba <anasbenba123@gmail.com>
2024-01-09 04:25:16 -0800

    fix code

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,16 +4,10 @@ to display your id"""
 import requests
 import sys
 
-if __name__ == '__main__':
-    url = f'https://api.github.com/repos/{sys.argv[1]}/{sys.argv[2]}/commits'
-    responde = requests.get(url)
-    r = responde.json()
-    j = 0
-    for i in range(len(r)):
-        for j in range(0, j-i-1):
-            date1 = int(r[j]['date'].split('T')[0].split('/', ''))
-            date2 = int(r[j+1]['date'].split('T')[0].split('/', ''))
-            if date1 < date2:
-                r[j], r[j+1] = r[j+1], r[j]
-    for i in range(0, 10):
-        print(f"{r[i]['sha']}: {r[i]['commit']['author']['name']}")
+
+if __name__ == "__main__":
+    resp = requests.get('https://api.github.com/repos/{}/{}/commits'
+                        .format(sys.argv[2], sys.argv[1]))
+    for commit in resp.json()[0:10]:
+        print("{}: {}".format(commit.get('sha'),
+                              commit.get('commit').get('author').get('name')))
