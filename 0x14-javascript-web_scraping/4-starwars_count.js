dbc034210108e82a96664ccef286641b8e8cea53
AnasBenba <anasbenba123@gmail.com>
2024-01-30 15:23:35 +0100

    add task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -6,9 +6,7 @@ const apiUrl = process.argv[2];
 const characterId = 18;
 
 request.get(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else {
+  if (!error) {
     try {
       const filmsData = JSON.parse(body).results;
       const moviesWithWedgeAntilles = filmsData.filter((film) =>
