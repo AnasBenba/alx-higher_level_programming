85854189736099f48be9e64bed5a3d0efdbe31f7
AnasBenba <anasbenba123@gmail.com>
2024-01-30 15:28:26 +0100

    add task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,32 +2,25 @@
 
 const request = require('request');
 
-const makeRequest = (apiUrl) => {
-  return new Promise((resolve, reject) => {
-    request(apiUrl, (error, response, body) => {
-      if (error) {
-        reject(error);
-      } else {
-        resolve(body);
-      }
-    });
+const makeRequest = apiUrl => new Promise((resolve, reject) => {
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve(body);
+    }
   });
-};
+});
 
-const countMoviesWithWedge = (results) => {
-  return results.reduce((count, movie) => {
-    return movie.characters.find((character) => character.endsWith('/18/'))
-      ? count + 1
-      : count;
-  }, 0);
-};
+const countMoviesWithWedge = results => results.reduce((count, movie) => (
+  movie.characters.find(character => character.endsWith('/18/'))
+    ? count + 1
+    : count
+), 0);
 
-// Main function
 const main = async () => {
   try {
-
     const apiUrl = process.argv[2];
-
     const body = await makeRequest(apiUrl);
     const results = JSON.parse(body).results;
 
