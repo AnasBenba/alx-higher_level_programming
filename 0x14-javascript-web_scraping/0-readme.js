d4d815070aeeea0efafdbdab16c5f2fe410eb7f5
AnasBenba <anasbenba123@gmail.com>
2024-01-30 15:10:07 +0100

    add task 0 and README.md file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs');
 
-// Check if the correct number of arguments is provided
 if (process.argv.length !== 3) {
   console.error('Usage: node 0-readme.js <file-path>');
   process.exit(1);
@@ -10,7 +9,6 @@ if (process.argv.length !== 3) {
 
 const filePath = process.argv[2];
 
-// Read the content of the file in utf-8
 fs.readFile(filePath, 'utf-8', (err, data) => {
   if (err) {
     console.error(err);
---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node 1-writeme.js <file-path> <string-to-write>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+const stringToWrite = process.argv[3];
+
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(`${filePath} has been successfully written.`);
+  }
+});
